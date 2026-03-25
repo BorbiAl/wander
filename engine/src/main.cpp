8e4a55d2da2ef54c1aab581cd777985330c3ac26
BorisAl. <boris.v.aleksandrov.2023@elsys-bg.org>
2026-03-26 00:23:42 +0200

    Refactor code structure for improved readability and maintainability

--- engine/src/main.cpp
+++ engine/src/main.cpp
@@ -1,3 +1,17 @@
+#include "httplib.h"
+#include "json.hpp"
+
 int main() {
+    httplib::Server svr;
+
+    svr.Get("/ping", [](const httplib::Request&, httplib::Response& res) {
+        nlohmann::json body = {
+            {"status", "ok"}
+        };
+        res.set_content(body.dump(), "application/json");
+        res.status = 200;
+    });
+
+    svr.listen("0.0.0.0", 8081);
     return 0;
 }
